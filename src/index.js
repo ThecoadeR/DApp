5ff312f72c4507343fdf337eb8b4ed28e5e9e213
zhh <406132631@qq.com>
2022-07-18 16:23:09 +0800

    feat: init project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import React from 'react'
+import ReactDOM from 'react-dom'
+// import { render } from 'react-dom'
+// import { Provider } from 'react-redux'
+// import 'bootstrap/dist/css/bootstrap.css' /* 顺序要在 App 之前, 不然它会覆盖 App.css 样式 */
+import App from './App'
+// import configureStore from './store/configureStore'
+
+// const store = configureStore()
+
+ReactDOM.render(
+  // <Provider store={configureStore()}>
+    <App />,
+  // </Provider>,
+  document.getElementById('root')
+)
